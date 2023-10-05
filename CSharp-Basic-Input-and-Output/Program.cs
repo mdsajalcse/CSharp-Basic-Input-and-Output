b3462d6df3619f1c1b591e85fe2ae5a085c9d286
sajal <sajalcse2018@gmail.com>
2023-10-05 11:12:28 +0600

    Add project files.

---
+++ CSharp-Basic-Input-and-Output/Program.cs
@@ -0,0 +1,8 @@
+﻿// C# Basic Input and Output
+
+//How to use WriteLine() and Write() method
+
+Console.WriteLine("C# Basic Input and Output");
+Console.Write("C# Basic Input");
+Console.Write(" and Output");
+
