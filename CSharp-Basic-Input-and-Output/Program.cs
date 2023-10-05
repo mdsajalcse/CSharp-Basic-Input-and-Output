927a99b13529f5e963e40f83fb1088d868b0c739
sajal <sajalcse2018@gmail.com>
2023-10-05 16:18:35 +0600

    input output

--- CSharp-Basic-Input-and-Output/Program.cs
+++ CSharp-Basic-Input-and-Output/Program.cs
@@ -1,8 +1,17 @@
 ﻿// C# Basic Input and Output
 
+
+// Printing String using WriteLine()
+Console.WriteLine("Hello C#");
+
 //How to use WriteLine() and Write() method
 
 Console.WriteLine("C# Basic Input and Output");
 Console.Write("C# Basic Input");
 Console.Write(" and Output");
 
+//Printing Variables and Literals using WriteLine() and Write()
+Console.WriteLine("Printing Variables and Literals using WriteLine() and Write()");
+int Value = 50;
+Console.WriteLine(Value);
+Console.WriteLine(50.50);?
